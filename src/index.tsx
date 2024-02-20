6f4b56fea5b1c68a13482ea1bb8e26fbe709ca08
LUIS_RODRIGUEZ <luisdavidrodriguezv@gmail.com>
2024-02-20 17:48:57 -0600

    Install linters

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { CssBaseline } from '@mui/material';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <CssBaseline>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </CssBaseline>
   </React.StrictMode>
 );
 
